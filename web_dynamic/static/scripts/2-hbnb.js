5d089ba57ef4222a934e74f7912f151e1af3984a
20Emi <iop.emilysanchez1@gmail.com>
2023-09-27 06:51:37 -0700

    update task 2 in js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,26 @@
 #!/usr/bin/node
 $(document).ready(function () {
-    const amenities = {};
+  let amenities = {};
 
-    $('input[type="checkbox"]').change(function () {
-        if ($(this).is(':checked')) {
-            amenities[$(this).data('name')] = $(this).data('id');
-        } else {
-            delete amenities[$(this).data('name')];
-        }
-        const idsamenities = Object.keys(amenities);
-        const listamenities = idsamenities.join(', ');
-        $(' .amenities h4').text(listamenities);
-    });
-	$.get('http://localhost:5001/api/v1/status/', function (data, textStatus) {
-	  if (textStatus === 'success' && data.status === 'OK') {
-	      $("#api_status").addClass('available');
-	        } else {
-			$("#api_status").removeClass('available');
-	           }
-	           });
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let idsamenities = Object.values(amenities);
+    let listamenities = idsamenities.join(', ');
+    if (listamenities.length > 30) {
+      listamenities = listamenities.substring(0, 30) + '...';
+    }
+    listamenities += '&nbsp;'
+    $('.amenities h4').html(listamenities);
+  });
+  $.get('http://localhost:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success' && data.status === 'OK') {
+      $("#api_status").addClass('available');
+    } else {
+      $("#api_status").removeClass('available');
+    }
+  });
 });
